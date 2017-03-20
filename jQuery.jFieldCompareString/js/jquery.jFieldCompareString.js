da89be329842717e4815de86e56535a5b9d5d246
lluz <luisluz@gmail.com>
2017-03-20 02:18:01 +0000

    update projs

--- jQuery.jFieldCompareString/js/jquery.jFieldCompareString.js
+++ jQuery.jFieldCompareString/js/jquery.jFieldCompareString.js
@@ -10,7 +10,7 @@
  * Project home:
  *   https://github.com/lluz/jquery-field-compare-string
  *
- * Version:  1.0.1
+ * Version:  1.0.2
  *
  */
 
@@ -18,7 +18,7 @@
 
     $.fn.jFieldCompareString = function(options) {
         if ( this === undefined || this.length === 0 || this.length > 1 ) {
-            console.log('jQuery.jFieldCompareString() INITIALIZATION ERROR: This is a 1:1 plugin initialization. You need to select only one input/textarea and declare the reference text element with the options property "compare_to". See documentation form more information.');
+            console.log('jquery.jFieldCompareString() INITIALIZATION ERROR: This is a 1:1 plugin initialization. You need to select only one input/textarea and declare the reference text element with the options property "compare_to". See documentation form more information.');
             return false;
         }
 
@@ -123,7 +123,7 @@
             else {
                 var compareElement = $(options.compare_to);
                 if ( !compareElement.length ) {
-                    console.log('jQuery.jFieldCompareString() INITIALIZATION ERROR: You need to declare the reference text element with the options property "compare_to". See documentation form more information.');
+                    console.log('jquery.jFieldCompareString() INITIALIZATION ERROR: You need to declare the reference text element with the options property "compare_to". See documentation form more information.');
                     return false;
                 }
                 else {
